ff4cb5686eeb75ddb004dad28cc70319ea7f1067
Aleksey <alfakher_tab@mail.ru>
2023-10-07 18:13:46 +0300

    Initialize project using Create React App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import "./_base.scss";
+import "./_vars.scss";
+import './_reset.scss';
+import './section-cart.scss.scss'
+
+import Title from "../Title/Title";
+import Cart from "../Cart/Cart";
+
+
+function App() {
+  return (
+    <div className="App">
+        <section className="section-cart">
+            <header className="section-cart__header">
+                <div className="container">
+                    <Title/>
+                </div>
+            </header>
+            <div className="section-cart__body">
+                <div className="container">
+
+                    <Cart/>
+
+                </div>
+            </div>
+        </section>
+    </div>
+  );
+}
+
+export default App;
